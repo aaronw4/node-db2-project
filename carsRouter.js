ca9c2d083f039c546ef0ad2cf2931bf68747da7e
Aaron Williams <aaronrw4@yahoo.com>
2020-01-31 21:01:53 -0800

    Created get and post. Both work

--- carsRouter.js
+++ carsRouter.js
@@ -12,7 +12,31 @@ router.get('/', (req, res) => {
             res.status(500).json({
                 error: 'Failed to retrieve cars.'
             })
-        })
+        });
 })
+
+router.post('/', (req, res) => {
+    const {VIN, make, model, mileage} = req.body;
+
+    if (!VIN || !make || !model || !mileage) {
+        res.status(400).json({
+            error: 'Must provide VIN, make, model, and mileage.'
+        })
+    }
+
+    db('cars').insert(req.body)
+        .then(ids => {
+            db('cars').where({id: ids[0]})
+                .then(newCar => {
+                    res.status(201).json(newCar)
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'Failed to store data.'
+            });
+        });
+
+});
 
 module.exports = router;
