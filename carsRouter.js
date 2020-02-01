19d69027dd3a10f786c8f97ad89448c57cabebec
Aaron Williams <aaronrw4@yahoo.com>
2020-01-31 21:18:50 -0800

    put request works

--- carsRouter.js
+++ carsRouter.js
@@ -39,4 +39,29 @@ router.post('/', (req, res) => {
 
 });
 
+router.put('/:id', async (req, res) => {
+    const {VIN, make, model, mileage} = req.body;
+
+    if (!VIN || !make || !model || !mileage) {
+        res.status(400).json({
+            error: 'Must provide VIN, make, model, and mileage.'
+        })
+    }
+    if (!req.params.id) {
+        res.status(400).json({
+            error: 'Must include an id.'
+        })
+    }
+    try {
+        const carUpdate = await db('cars')
+            .where('id', req.params.id)
+            .update(req.body);
+        res.status(200).json({updated: carUpdate});
+    } catch {
+        res.status(500).json({
+            error: 'Failed to update car.'
+        })
+    }
+})
+
 module.exports = router;
